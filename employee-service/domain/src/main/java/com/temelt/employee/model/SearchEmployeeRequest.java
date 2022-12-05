5b705dd85b8845e756f00417e4b473b946584c85
Taner TEMEL <taner.temel@haiilo.com>
2022-12-05 11:21:04 +0100

    Spring boot 3.0.0 version update

--- employee-service/domain/src/main/java/com/temelt/employee/model/SearchEmployeeRequest.java
+++ employee-service/domain/src/main/java/com/temelt/employee/model/SearchEmployeeRequest.java
@@ -1,12 +1,11 @@
 package com.temelt.employee.model;
 
 import com.temelt.common.Scenario;
-import javax.validation.constraints.NotNull;
 
 public class SearchEmployeeRequest implements Scenario {
     private String id;
 
-    public SearchEmployeeRequest(@NotNull final String id) {
+    public SearchEmployeeRequest(final String id) {
         this.id = id;
     }
 
